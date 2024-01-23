b94ec3bd425ff6417257b3d8028d37305e246713
kiembully <kimklintonvale@gmail.com>
2024-01-23 15:06:17 +0800

    closure output

--- closures.rb
+++ closures.rb
@@ -1,10 +1,22 @@
 module Closures
-  def self.with_these_numbers(a, b)
+  def self.with_these_numbers(first, second, &block)
+    return [first, second] unless block_given?
+    block.call(first, second)
   end
 
-  def self.map(list)
+  def self.map(list, &block)
+    return list if !block_given?
+
+    mapped_list = []
+    list.each { |item| mapped_list << block.call(item)}
+    mapped_list
   end
 
-  def self.filter(list)
+  def self.filter(list, &block)
+    return list if !block_given?
+
+    filtered_list = []
+    list.each { |item| filtered_list << item if block.call(item)}
+    filtered_list
   end
 end
