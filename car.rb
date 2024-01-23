f35b53341378ec9d6a2918242f5df7f26950484b
fuentes-vincent <fuentes.vincent96@gmail.com>
2024-01-23 15:32:49 +0800

    Closure activity

--- car.rb
+++ car.rb
@@ -1,2 +1,19 @@
 class Car
+    attr_reader :color, :manufacturer, :plate_number
+
+    def initialize(attributes = {})
+        @color = attributes[:color]
+        @plate_number = attributes[:plate_number]
+        @manufacturer = attributes[:manufacturer]
+
+        yield(self) if block_given?
+    end
+
+    def plate_number=(value)
+        @plate_number = value
+    end
+
+    def manufacturer=(value)
+        @manufacturer = value
+    end
 end
--- closures.rb
+++ closures.rb
@@ -1,10 +1,33 @@
 module Closures
   def self.with_these_numbers(a, b)
+    if block_given?
+      yield(a, b)
+    else
+      [a, b]
+    end
   end
 
   def self.map(list)
+    if block_given?
+      new_list = []
+      list.each do |item|
+        new_list << yield(item)
+      end
+        new_list
+    else
+      list
+    end
   end
 
   def self.filter(list)
+    if block_given?
+      filtered_list = []
+      list.each do |item|
+        filtered_list << item if yield(item)
+      end
+        filtered_list
+    else
+      list
+    end
   end
 end
